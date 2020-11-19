5669192e0f0c39c6c15a23255dafe8d64752a17b
Walter <walter.santacruz@correounivalle.edu.co>
2020-11-19 16:20:12 -0500

    agregue carpeta prefaps y codigo para random

---
+++ TheGame/Assets/Scripts/RandomObject.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomObject : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        SetRandomPosition();
+    }
+
+    void SetRandomPosition(){
+    	float x = Random.Range(-5.0f,5.00f);
+    	Debug.Log("X: "+ x.ToString("F2"));
+    	transform.position = new Vector3(x, 0.5f, transform.position.z);
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
